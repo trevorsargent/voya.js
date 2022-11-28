37654ae2b8da2644e76e0d55f177cfbe1f1b2abc
Trevor Sargent <trevor.a.sargent@gmail.com>
2022-11-27 19:36:21 -0800

    feat: move to trpc

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,50 +1,6 @@
-/**
- * This is the API-handler of your app that contains all your API routes.
- * On a bigger app, you will probably want to split this file up into multiple files.
- */
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
-import { NextRequest } from "next/server"
-import { z } from "zod"
-import { publicProcedure, router } from "../../server/trpc"
+import { createNextApiHandler } from "@trpc/server/adapters/next"
+import { appRouter } from "../../lib/trpc/trpc.server"
 
-const appRouter = router({
-  greeting: publicProcedure
-    // This is the input schema of your procedure
-    // 💡 Tip: Try changing this and see type errors on the client straight away
-    .input(
-      z.object({
-        name: z.string().nullish(),
-      })
-    )
-    .query(({ input }) => {
-      // This is what you're returning to your client
-      return {
-        text: `hello ${input?.name ?? "world"}`,
-        // 💡 Tip: Try adding a new property here and see it propagate to the client straight-away
-      }
-    }),
-  // 💡 Tip: Try adding a new procedure here and see if you can use it in the client!
-  // getUser: t.procedure.query(() => {
-  //   return { id: '1', name: 'bob' };
-  // }),
+export default createNextApiHandler({
+  router: appRouter,
 })
-
-// export only the type definition of the API
-// None of the actual implementation is exposed to the client
-export type AppRouter = typeof appRouter
-
-// We're using the edge-runtime
-export const config = {
-  runtime: "experimental-edge",
-}
-
-// export API handler
-export default async function handler(req: NextRequest) {
-  console.log("HI")
-  return fetchRequestHandler({
-    endpoint: "api/trpc",
-    router: appRouter,
-    req,
-    createContext: () => ({}),
-  })
-}
